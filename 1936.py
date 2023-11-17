0dbb73956389018cec741cd6b3c2c9c7a32f3b88
withtaylors <withtaylors@naver.com>
2023-11-18 04:57:15 +0900

    1936

--- 1936.py
+++ 1936.py
@@ -2,10 +2,18 @@ import sys;
 sys.stdin = open('input.txt', 'r')
 
 # 무슨 가위바위보 낼건지 받기 - 빈칸으로 나누기
-a, b = list(map(int, input().split()))
+A, B = list(map(int, input().split()))
 
 # 누가 이겼는지 판별하는 과정 작성
-if (a-b) == -1 or (a-b) == 2:
-    print('B')
-else:
+# if (a-b) == -1 or (a-b) == 2:
+#     print('B')
+# else:
+#     print("A")
+if (A == 1 and B == 3):
     print("A")
+elif (A == 3 and B == 1):
+    print("B")
+elif (A<B):
+    print('B')
+elif (A>B):
+    print('A')
