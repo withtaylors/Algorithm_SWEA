62ff833e37fec23e173013808318f517f8cc5777
withtaylors <withtaylors@naver.com>
2023-11-17 21:13:54 +0900

    2071 수정

--- 2071.py
+++ 2071.py
@@ -4,9 +4,11 @@ T = int(input())
 #테스트 케이스 각각 처리하기
 for test_case in range(1, T+1):
     nums = list(map(int, input().split()))
-    print(nums)
+    #print(nums)
     #[3, 17, 1, 39, 8, 41, 2, 32, 99, 2]
+
     avg_value = round(sum(nums)/len(nums))
-    print(avg_value)
+    #print(avg_value)
     #24
+
     print("#{} {}".format(test_case, avg_value))
